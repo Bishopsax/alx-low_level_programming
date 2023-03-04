d91991434233bc56067ea54437122fcf7e2429ea
Bishopsax <wisdomsunday121@gmail.com>
2023-03-04 02:34:32 -0800

    putchar file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,19 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * main - Print _putchar
+ * Return: Always 0
  */
 int main(void)
 {
-	char i[8] = "_putchar";
-
-	{
-		printf("%s", i);
-	}
-	printf("\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
